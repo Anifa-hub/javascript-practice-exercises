120cc582c4fadb40eb978358caa25a8266b0a9de
anifa <anifabazubagira@gmail.com>
2024-11-13 17:19:29 +0200

    changing from fahreneit to celsius

--- 01-fundamentals/variables/temperatureConverter.js
+++ 01-fundamentals/variables/temperatureConverter.js
@@ -1,3 +1,6 @@
 let celsius = 34
 let fahreneit = (celsius * 9/5 + 32)
 console.log(`${celsius} NN°C is ${ fahreneit} NN°F`)
+  fahreneit = 93 
+   celsius = (fahreneit-32) * 5/9
+console.log(`${fahreneit} NN°F is ${ celsius} NN°C `)
