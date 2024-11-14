8e992a5cbac6aa2ada33b3e380ab12f70218c468
anifa <anifabazubagira@gmail.com>
2024-11-14 15:11:50 +0200

    This shows student's grades

---
+++ 01-fundamentals/conditionals/exercise7.js
@@ -0,0 +1,20 @@
+let numbers = [77,88,95,68];
+let sum = 0;
+for ( i=0; i<numbers.length; i++){
+   sum += numbers[i] 
+}
+let average = sum / numbers.length
+console.log(average)
+if (average <60){
+    console.log('F')
+} else if (average<70){
+    console.log('D')
+} else if (average<80){
+    console.log('C')
+} else if (average<90){ 
+    console.log('B')
+}else if (average<100){
+    console.log('A')
+} else {
+    console.log('Non')
+}
