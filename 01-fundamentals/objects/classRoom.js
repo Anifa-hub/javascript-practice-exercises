6a85d29e3fd581a2132e4140b1b7602bc741043f
anifa <anifabazubagira@gmail.com>
2024-11-15 15:31:59 +0200

    This for object

---
+++ 01-fundamentals/objects/classRoom.js
@@ -0,0 +1,15 @@
+const classRoom=[
+    {
+        id:1,
+        fristName:"John"
+    },
+    {
+        id:2,
+        fristName:"Doe", 
+    },
+    {
+        id:3,
+        fristName:"Anifa",
+    }
+]
+console.log(classRoom[1].id)
---
+++ 01-fundamentals/objects/person.js
@@ -0,0 +1,14 @@
+const person={
+    firstName:"Anifa",
+    lastName:"Bazubagira",
+    age:17
+}
+person.gender="Female"
+console.log(person);
+// here is student
+const student={
+    name:"Anifa Bazubagira",
+    studentYear:"B1" ,
+    rollno:23
+}
+console.log(student);
