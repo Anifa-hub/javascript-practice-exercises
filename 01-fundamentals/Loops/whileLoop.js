61200fc9c992699f85cfed42cd1de9165d25e797
anifa <anifabazubagira@gmail.com>
2024-11-15 14:18:14 +0200

    this is loops

--- 01-fundamentals/Loops/whileLoop.js
+++ 01-fundamentals/Loops/whileLoop.js
@@ -1,7 +1,13 @@
-const number=[1,2,3,4,5];
-//let finalIndex=number.length-1;
-let n=4;
+let number=[1,2,3,4,5];
+let n=0;
 do{
     console.log(number[n]);
-    n--;
-}while(n>=0)
+    n++;
+}while(n<number.length)
+    // the synatax of while loop
+    let num = [1,2,3,4,5]
+    let i =0
+    while (i< num.length){
+        console.log(num[i])
+        i++
+    }
