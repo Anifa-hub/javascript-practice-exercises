2aeb28053d68371723e26da5af1e9f23b7bf8a6a
anifa <anifabazubagira@gmail.com>
2024-11-15 17:53:54 +0200

    This contain while and do while loops

--- 01-fundamentals/Loops/whileLoop.js
+++ 01-fundamentals/Loops/whileLoop.js
@@ -5,7 +5,7 @@ do{
     n++;
 }while(n<number.length)
     // the synatax of while loop
-    let num = [1,2,3,4,5]
+    let num = [1,3,4,5]
     let i =0
     while (i< num.length){
         console.log(num[i])
